b341ffcf255c1ab8de4b72d23fdcdd526f10ae30
Indra-Poudel <indra.poudel1998@gmail.com>
2024-08-02 11:42:58 +0545

    timeline poc

--- src/components/Skia/TimelineView.tsx
+++ src/components/Skia/TimelineView.tsx
@@ -1,3 +1,4 @@
+import Interval from 'components/Timeline/Interval';
 import React from 'react';
 import {View, useWindowDimensions} from 'react-native';
 import {Gesture, GestureDetector} from 'react-native-gesture-handler';
@@ -5,15 +6,12 @@ import Animated, {
   SharedValue,
   useAnimatedReaction,
   useAnimatedStyle,
-  useDerivedValue,
   useSharedValue,
 } from 'react-native-reanimated';
 import {useTheme} from 'theme/ThemeContext';
-import {GeneratedSentence} from 'utils/sentencesBuilder';
 
 type TimelineProps = {
   currentTime: SharedValue<number>;
-  sentences: GeneratedSentence[];
   frameRate: number;
   totalDuration: SharedValue<number>;
   seek: SharedValue<number>;
@@ -22,7 +20,6 @@ type TimelineProps = {
 const Timeline = ({
   frameRate,
   currentTime,
-  sentences,
   seek,
   totalDuration,
 }: TimelineProps) => {
@@ -32,8 +29,12 @@ const Timeline = ({
   const widthPerMs = 0.5;
 
   const totalWidth = totalDuration.value * widthPerMs;
-  const numberOfIntervals = Math.ceil(totalDuration.value / intervalDurationMs);
-  const widthPerInterval = totalWidth / numberOfIntervals;
+  const numberOfIntervals = Math.floor(
+    totalDuration.value / intervalDurationMs,
+  );
+  const partialIntervalWidth =
+    (totalDuration.value % intervalDurationMs) * widthPerMs;
+  const widthPerInterval = intervalDurationMs * widthPerMs;
   const widthPerFrame = widthPerInterval / framesPerInterval;
 
   const {width} = useWindowDimensions();
@@ -44,6 +45,21 @@ const Timeline = ({
     () => currentTime.value,
     _currentTime => {
       x.value = -_currentTime * widthPerMs;
+
+      if (x.value >= 0) {
+        x.value = 0;
+      }
+
+      if (_currentTime + frameDurationMs >= totalDuration.value) {
+        x.value = -_currentTime * widthPerMs;
+      }
+
+      if (
+        x.value <=
+        -(totalDuration.value - frameDurationMs * 2) * widthPerMs
+      ) {
+        x.value = -(totalDuration.value - frameDurationMs * 2) * widthPerMs;
+      }
     },
     [currentTime],
   );
@@ -52,28 +68,32 @@ const Timeline = ({
     .onChange(e => {
       x.value += e.changeX;
 
-      const newSeekValue = -x.value / widthPerMs;
-      if (newSeekValue >= 0 && newSeekValue <= totalDuration.value) {
-        seek.value = newSeekValue;
-      }
-
-      // Ensure x.value does not exceed the bounds
-      if (x.value > 0) {
+      if (x.value >= 0) {
         x.value = 0;
       }
-    })
-    .onEnd(() => {});
 
-  const animatedStyles = useAnimatedStyle(() => {
-    return {
-      transform: [{translateX: width / 2 + x.value}],
-    };
-  }, [currentTime]);
+      if (
+        x.value <=
+        -(totalDuration.value - frameDurationMs * 2) * widthPerMs
+      ) {
+        x.value = -(totalDuration.value - frameDurationMs * 2) * widthPerMs;
+      }
+
+      const newSeekValue = -x.value / widthPerMs;
+      seek.value = newSeekValue;
+    })
+    .onEnd(() => {
+      // // Snap to nearest frame or interval
+      // seek.value = Math.round(seek.value / frameDurationMs) * frameDurationMs;
+    });
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{translateX: width / 2 + x.value}],
+  }));
 
   return (
     <GestureDetector gesture={gesture}>
       <View style={{height: 150, backgroundColor: theme.colors.black1}}>
-        {/* Total timeline */}
         <Animated.View
           style={[
             {
@@ -83,59 +103,48 @@ const Timeline = ({
             },
             animatedStyles,
           ]}>
-          {/* Draw the intervals and frames */}
-          {[...Array(numberOfIntervals)].map((_, intervalIndex) => {
-            const intervalStartX = intervalIndex * widthPerInterval;
-
-            return (
-              <React.Fragment key={intervalIndex}>
-                {/* Draw a larger line at the start of the interval */}
+          {[...Array(numberOfIntervals)].map((_, intervalIndex) => (
+            <React.Fragment key={intervalIndex}>
+              <Interval
+                x={intervalIndex * widthPerInterval}
+                lineWidth={2}
+                label={`${((intervalIndex * intervalDurationMs) / 1000).toFixed(
+                  2,
+                )}s`}
+                lineColor={theme.colors.grey4}
+                height={40}
+              />
+              {[...Array(framesPerInterval)].map((_, frameIndex) => (
                 <View
+                  key={frameIndex}
                   style={{
                     position: 'absolute',
-                    left: intervalStartX,
-                    top: 10,
-                    width: 2,
-                    height: 40,
-                    backgroundColor: 'black',
+                    left:
+                      intervalIndex * widthPerInterval +
+                      frameIndex * widthPerFrame,
+                    top: 20,
+                    width: 1,
+                    height: 20,
+                    backgroundColor: 'grey',
                   }}
                 />
-                {/* Draw the frames within the interval */}
-                {[...Array(framesPerInterval)].map((_, frameIndex) => {
-                  const frameStartX =
-                    intervalStartX + frameIndex * widthPerFrame;
-                  return (
-                    <View
-                      key={frameIndex}
-                      style={{
-                        position: 'absolute',
-                        left: frameStartX,
-                        top: 20,
-                        width: 1,
-                        height: 20,
-                        backgroundColor: 'grey',
-                      }}
-                    />
-                  );
-                })}
-                {/* Draw a larger line at the end of the interval */}
-                <View
-                  style={{
-                    position: 'absolute',
-                    left: intervalStartX + widthPerInterval,
-                    top: 10,
-                    width: 2,
-                    height: 40,
-                    backgroundColor: 'black',
-                  }}
-                />
-              </React.Fragment>
-            );
-          })}
+              ))}
+            </React.Fragment>
+          ))}
+          {/* Handle partial interval if exists */}
+          {partialIntervalWidth > 0 && (
+            <Interval
+              x={numberOfIntervals * widthPerInterval}
+              lineWidth={2}
+              label={`${(
+                (numberOfIntervals * intervalDurationMs) /
+                1000
+              ).toFixed(2)}s`}
+              lineColor={theme.colors.grey4}
+              height={40}
+            />
+          )}
         </Animated.View>
-        {/* end of total timeline */}
-
-        {/* current time indicator  */}
         <View
           style={{
             position: 'absolute',
@@ -168,7 +177,6 @@ const Timeline = ({
             }}
           />
         </View>
-        {/*  end of current time indicator  */}
       </View>
     </GestureDetector>
   );
